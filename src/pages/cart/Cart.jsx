7767d90bcba971322d8b30d293bdd7ea7627117d
Sandeep Kumar Jena <sandeepkumar637053@gmail.com>
2023-12-31 08:15:13 +0530

    update payment integartion

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -98,7 +98,7 @@ function Cart() {
       amount: parseInt(grandTotal * 100),
       currency: "INR",
       order_receipt: 'order_rcptid_' + name,
-      name: "E-Bharat",
+      name: "EasyBazaar",
       description: "for testing purpose",
       handler: function (response) {
         console.log(response)
