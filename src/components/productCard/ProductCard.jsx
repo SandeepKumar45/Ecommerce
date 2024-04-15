2bc6fae566acabe7b50a22538494f50896cdf246
Sandeep Kumar Jena <sandeepkumar637053@gmail.com>
2024-04-15 20:50:40 +0530

    update cart

--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -34,7 +34,7 @@ function ProductCard() {
                         .filter((obj) => obj.price.includes(filterPrice)).slice(0, 8).map((item, index) => {
                             const { title, price, imageUrl, id } = item;
                             return (
-                                <div key={index} className="p-4 w-[65%] md:w-1/4  drop-shadow-lg " >
+                                <div key={index} className="p-4  w-full sm:w-[65%] md:w-1/4  drop-shadow-lg " >
                                     <div className="h-full border-2 hover:shadow-gray-100 hover:shadow-2xl transition-shadow duration-300 ease-in-out    border-gray-200 border-opacity-60 rounded-2xl overflow-hidden" style={{ backgroundColor: mode === 'dark' ? 'rgb(46 49 55)' : null, color: mode === 'dark' ? 'white' : null, }} >
                                         <div onClick={() => navigate(`/productinfo/${id}`)} className="flex justify-center cursor-pointer" >
                                             <img className=" rounded-2xl w-full h-80 p-2 hover:scale-110 transition-scale-110  duration-300 ease-in-out" src={imageUrl} alt="blog" />
